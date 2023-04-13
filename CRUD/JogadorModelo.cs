bf40c0c14f50829759972e3ffbbddf14a575ce40
Isaque Amorim <isaque.amorim@inventsoftware.com.br>
2023-04-13 11:12:53 -0300

    Criando Modelo

---
+++ CRUD/JogadorModelo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    public class JogadorModelo
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string Elo { get; set; }
+        public DateTime DataNascimento { get; private set; }
+
+        public JogadorModelo(string Nome,string Sobrenome,string NickName,string Email,string Elo,DateTime DataNascimento)
+        {
+            this.Nome = Nome;
+            this.LastName = Sobrenome;
+            this.NickName = NickName;
+            this.Email = Email;
+            this.Elo = Elo;
+            this.DataNascimento = DataNascimento;
+        }
+
+    }
+}
