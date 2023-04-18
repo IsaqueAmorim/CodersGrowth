5b752385b2607c96137af9d5770b3b1cd00e1c5d
Isaque Amorim <isaqueamorim25@gmail.com>
2023-04-18 19:48:00 -0300

    removendo usings desnecessários

--- CRUD/FML_Listagem.cs
+++ CRUD/FML_Listagem.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CRUD
+﻿namespace CRUD
 {
     public partial class FML_Listagem : Form
     {
