f481fd4f2f5778d09d0336505349ac0bd36b5b3d
Isaque Amorim <isaqueamorim25@gmail.com>
2023-04-27 22:17:27 -0300

    Identando melhor o código

--- CRUD.DOMAIN/Modelos/JogadorModelo.cs
+++ CRUD.DOMAIN/Modelos/JogadorModelo.cs
@@ -15,11 +15,13 @@ namespace CRUD.Modelos
         [Column("Nome")]
         public string Nome { get; set; }
 
+
         [NotNull("O Sobrenome não pode ser nulo!")]
         [MaxLength(50, ErrorMessage = "O Sobrenome pode conter no máximo 50 caracteres")]
         [MinLength(2, ErrorMessage = "O Sobrenome deve ter no mínimo 2 caracteres!")]
         [Column("Sobrenome")]
         public string Sobrenome { get; set; }
+
 
         [NotNull("O Apelido não pode ser nulo!")]
         [MaxLength(50, ErrorMessage = "O Apelido pode conter no máximo 50 caracteres")]
@@ -27,18 +29,25 @@ namespace CRUD.Modelos
         [Column("Apelido")]
         public string Apelido { get; set; }
 
+
         [NotNull("O Email não pode ser nulo!")]
         [MaxLength(70, ErrorMessage = "O Email pode conter no máximo 70 caracteres")]
         [MinLength(2, ErrorMessage = "O Eamil deve ter no mínimo 2 caracteres!")]
         [Column("Email")]
         public string Email { get; set; }
 
+
         [Column("Elo")]
         public Elo Elo { get; set; }
+
+
         [Column("Data_de_Nascimento")]
         public DateTime DataNascimento { get; set; }
+
+
         [Column("Data_de_Criacao")]
         public DateTime DataCriacao { get; set; }
+
 
         public JogadorModelo(string nome, string sobrenome, string apelido, string email, Elo elo, DateTime dataNascimento)
         {
